b107ed0340ba3088c03e9e2e84c3dce2c8c7094c
scottbaczewski <130002237+scottbaczewski@users.noreply.github.com>
2025-04-08 15:40:02 -0400

    Complete exercises 7 and 8. Deff making sense

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //x °F ≘ (x − 32) × ⁠5/9⁠ °C
+  return Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //x °C ≘ (x × ⁠9/5 + 32) °F
+  return Math.round((celsius * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
